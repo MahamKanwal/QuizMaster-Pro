a4dfcd92645fbff037ea0d9dc72d53e4de64e4df
Maham Kanwal <kanwalmahsm@gmail.com>
2025-09-03 05:04:55 -0700

    more pages added

---
+++ app.js
@@ -0,0 +1,103 @@
+
+// let computerGuess = Math.floor(Math.random() * 10);
+// console.log(computerGuess);
+// let userGuess = prompt("enter a number from 0 to 10");
+// let tries = 1;
+
+// while (userGuess != computerGuess){
+//     userGuess = prompt("try again");
+//     tries++;
+// }
+// console.log(`you are win computer guess is ${computerGuess} you win in ${tries} tries`);
+
+// const num1 = (10;
+// const num2 = 40;
+// const num3 = 90;
+
+// if(num1 > num2 && num1 > num3){
+//     console.log("num1 is greatest",num1)
+// }
+// else if(num2 > num1 && num2 > num3){
+//     console.log("num2 is greatest",num2)
+// }
+// else{
+//     console.log("num3 is greatest",num3)
+// }
+
+// Comparison & Logical Operators
+// Ek variable marks lo aur uski value prompt se input karao.
+// Agar marks >= 90 hai to "Excellent" print karo,
+// agar marks >= 50 && marks < 90 hai to "Pass",
+// warna "Fail".
+
+// let marks = Number(prompt("Enter your marks:"));
+// if(marks >= 90){
+//     console.log("Excellent");
+// }
+// else if(marks >= 50 && marks < 90){
+// console.log("Pass");
+// }
+// else{
+//     console.log("Fail");
+// }
+
+const num1 = 10;
+const num2 = 40;
+const num3 = 90;
+// if(num1 < num2 && num1 < num3){
+//     console.log("num1 is smallest num2 and num 3")
+// }
+// else if(num2 < num1 && num2 < num3){
+//      console.log("num2 is smallest num3 and num1")
+// }
+// else{
+//     {
+//      console.log("num3 is smallest num2 and num1")
+// }
+// }
+
+// if (num1 > num2 && num1 > num3) {
+//     console.log("num 1 is greatest");
+//     if (num2 > num3) {
+//         console.log("num3 is smallest");
+//     } else {
+//         console.log("num2 is smallest");
+//     }
+// }
+// else if (num2 > num1 && num2 > num3) {
+//     console.log("num2 is greatest");
+//     if (num1 > num3) {
+//         console.log("num3 is smallest")
+//     } else {
+//         console.log("num1 is smaillest")
+//     }
+// }
+// else {
+//     console.log("num 3 is greatest");
+//     if (num1 > num2) {
+//         console.log("num1 is smallest")
+//     } else {
+//         console.log("num2 is smallest")
+//     }
+// }
+
+// const findGreatest = (numbers) => {
+//     let greatest = numbers[0];
+//     for (let num of numbers){
+//         if (num > greatest) {
+//             greatest = num;
+//         }
+// }
+// }
+
+
+// findGreatest([1, 3, 2, 5, 3, 5])
+// const totalNum = (numbers) =>{
+//      let total = 0;
+// for (let num of numbers){
+//     total+=num
+// }
+// console.log(total)
+// }
+// totalNum([1, 3, 2, 5, 3, 5])
+
